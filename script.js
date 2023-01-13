8f2bcf8c90b1b8a929aea6236f5b27031b91da68
Tawaccul <teps2222@gmail.com>
2023-01-13 23:46:30 +0400

    img editor

---
+++ script.js
@@ -0,0 +1,4 @@
+const fileInput = document.querySelector('.file-input')
+const chooseImgBtn = document.querySelector('.choose-img');
+
+chooseImgBtn.addEventListener('click' , () => fileInput.click());
